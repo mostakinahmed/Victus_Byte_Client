f963453789e6aca9994a0cee789dc31f479ed267
mostakinahmed <mostakin4556@gmail.com>
2026-03-04 08:28:35 +0600

    update my order

--- src/components/Profile/EditProfile.jsx
+++ src/components/Profile/EditProfile.jsx
@@ -117,7 +117,7 @@ function EditProfile({ data, checkUserStatus }) {
           <button
             type="submit"
             disabled={loading}
-            className="bg-[#1976d2] text-white px-10 py-3 rounded font-bold text-xs uppercase tracking-widest hover:bg-[#1565c0] transition-all shadow-lg shadow-blue-100 active:scale-95 disabled:bg-slate-300 disabled:cursor-not-allowed"
+            className="bg-[#1976d2] text-white px-10 py-3 rounded font-semibold  text-xs uppercase tracking-widest hover:bg-[#1565c0] transition-all shadow-lg shadow-blue-100 active:scale-95 disabled:bg-slate-300 disabled:cursor-not-allowed"
           >
             {loading ? "Processing..." : "Save Changes"}
           </button>
